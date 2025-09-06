12f79aa3317e8dd11f4b5a5965fe47a3b489d4cf
Jackson Isiko <isijackson@gmail.com>
2025-09-06 13:49:59 -0600

    Updated greeting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('HEllo IT 360 Classs, this is a sample APP'));
+app.get('/', (req, res) => res.send('HEllo IT 360 Classs, this is a sample APP, its as simple as 123'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
